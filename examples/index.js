5553051fa212206f05765e8a7e28f1d1284d2571
Guido García <guido.garciabernardo@telefonica.com>
2017-01-07 00:11:25 +0100

    Fix function name in example

--- examples/index.js
+++ examples/index.js
@@ -3,4 +3,4 @@ var client = require('../lib/client');
 
 var bing = new client.BingSpeechClient(process.env.MICROSOFT_BING_SPEECH_KEY);
 var wave = fs.readFileSync(__dirname + '/example.wav');
-bing.voiceRecognition(wave).then(result => console.log(result));
+bing.recognize(wave).then(result => console.log(result));
