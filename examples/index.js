fb2023457367464933c5c97828eb04d42ff145d0
Guido García <guido.garciabernardo@telefonica.com>
2017-01-13 12:53:39 +0100

    feat: add font voices support (lang + genre)

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,41 @@
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
 var client = require('../lib/client');
 
+if (!process.env.MICROSOFT_BING_SPEECH_KEY) {
+  console.error('You need to set a MICROSOFT_BING_SPEECH_KEY env var');
+  process.exit(1);
+}
+
 var bing = new client.BingSpeechClient(process.env.MICROSOFT_BING_SPEECH_KEY);
+
+/**
+ * Speech To Text example
+ */
 var wave = fs.readFileSync(__dirname + '/example.wav');
-bing.recognize(wave).then(result => console.log(result));
+bing.recognize(wave).then(result => {
+  console.log('Speech To Text completed');
+  console.log(result)
+  console.log('\n');
+});
+
+/**
+ * Text To Speech example (default lang & genre)
+ */
+bing.synthesize('All for one and one for all').then(result => {
+  var file = path.join(os.tmpdir(), 'bingspeech-api-client.wav');
+  var wstream = fs.createWriteStream(file);
+  wstream.write(result.wave);
+  console.log('Text To Speech completed. Audio file written to', file);
+});
+
+/**
+ * Text To Speech example (spanish female)
+ */
+bing.synthesize('Todos para uno y uno para todos', 'es-es', 'female').then(result => {
+  var file = path.join(os.tmpdir(), 'bingspeech-api-client.wav');
+  var wstream = fs.createWriteStream(file);
+  wstream.write(result.wave);
+  console.log('Text To Speech completed. Audio file written to', file);
+});
--- src/client.ts
+++ src/client.ts
@@ -9,6 +9,38 @@ const debug = require('debug')('bingspeechclient');
 
 const ASIAN_LOCALES = ['zh-cn', 'zh-hk', 'zh-tw', 'ja-jp'];
 
+const VOICES: { [key: string]: string } = {
+    'ar-eg female': 'Microsoft Server Speech Text to Speech Voice (ar-EG, Hoda)',
+    'de-de female': 'Microsoft Server Speech Text to Speech Voice (de-DE, Hedda)',
+    'de-de male': 'Microsoft Server Speech Text to Speech Voice (de-DE, Stefan, Apollo)',
+    'en-au female': 'Microsoft Server Speech Text to Speech Voice (en-AU, Catherine)',
+    'en-ca female': 'Microsoft Server Speech Text to Speech Voice (en-CA, Linda)',
+    'en-gb female': 'Microsoft Server Speech Text to Speech Voice (en-GB, Susan, Apollo)',
+    'en-gb male': 'Microsoft Server Speech Text to Speech Voice (en-GB, George, Apollo)',
+    'en-in male': 'Microsoft Server Speech Text to Speech Voice (en-IN, Ravi, Apollo)',
+    'en-us female': 'Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)',
+    'en-us male': 'Microsoft Server Speech Text to Speech Voice (en-US, BenjaminRUS)',
+    'es-es female': 'Microsoft Server Speech Text to Speech Voice (es-ES, Laura, Apollo)',
+    'es-es male': 'Microsoft Server Speech Text to Speech Voice (es-ES, Pablo, Apollo)',
+    'es-mx male': 'Microsoft Server Speech Text to Speech Voice (es-MX, Raul, Apollo)',
+    'fr-ca female': 'Microsoft Server Speech Text to Speech Voice (fr-CA, Caroline)',
+    'fr-fr female': 'Microsoft Server Speech Text to Speech Voice (fr-FR, Julie, Apollo)',
+    'fr-fr male': 'Microsoft Server Speech Text to Speech Voice (fr-FR, Paul, Apollo)',
+    'it-it male': 'Microsoft Server Speech Text to Speech Voice (it-IT, Cosimo, Apollo)',
+    'ja-jp female': 'Microsoft Server Speech Text to Speech Voice (ja-JP, Ayumi, Apollo)',
+    'ja-jp male': 'Microsoft Server Speech Text to Speech Voice (ja-JP, Ichiro, Apollo)',
+    'pt-br male': 'Microsoft Server Speech Text to Speech Voice (pt-BR, Daniel, Apollo)',
+    'ru-ru female': 'Microsoft Server Speech Text to Speech Voice (ru-RU, Irina, Apollo)',
+    'ru-ru male': 'Microsoft Server Speech Text to Speech Voice (ru-RU, Pavel, Apollo)',
+    'zh-cn female': 'Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)',
+    'zh-cn female': 'Microsoft Server Speech Text to Speech Voice (zh-CN, Yaoyao, Apollo)',
+    'zh-cn male': 'Microsoft Server Speech Text to Speech Voice (zh-CN, Kangkang, Apollo)',
+    'zh-hk female': 'Microsoft Server Speech Text to Speech Voice (zh-HK, Tracy, Apollo)',
+    'zh-hk male': 'Microsoft Server Speech Text to Speech Voice (zh-HK, Danny, Apollo)',
+    'zh-tw female': 'Microsoft Server Speech Text to Speech Voice (zh-TW, Yating, Apollo)',
+    'zh-tw male': 'Microsoft Server Speech Text to Speech Voice (zh-TW, Zhiwei, Apollo)'
+};
+
 // Official docs
 // STT https://www.microsoft.com/cognitive-services/en-us/speech-api/documentation/API-Reference-REST/BingVoiceRecognition
 // TTS https://www.microsoft.com/cognitive-services/en-us/speech-api/documentation/api-reference-rest/bingvoiceoutput
@@ -74,7 +106,7 @@ export class BingSpeechClient {
             });
     }
 
-    synthesize(text: string, locale: string = 'en-us'): Promise<VoiceSynthesisResponse> {
+    synthesize(text: string, locale: string = 'en-us', gender: string = 'female'): Promise<VoiceSynthesisResponse> {
         // see also https://github.com/Microsoft/Cognitive-Speech-TTS/blob/master/Samples-Http/NodeJS/TTSService.js
         return this.issueToken()
             .then((token) => {
@@ -88,11 +120,13 @@ export class BingSpeechClient {
                     text = this.convertToUnicode(text);
                 }
 
-                // TODO match name and locale
-                let name = 'Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)';
-                let gender = 'Female';
+                let font = voiceFont(locale, gender);
+                if (!font) {
+                    throw new Error(`No voice font for lang ${locale} and gender ${gender}`);
+                }
+
                 let ssml = `<speak version='1.0' xml:lang='${locale}'>
-                            <voice name='${name}' xml:lang='${locale}' xml:gender='${gender}'>${text}</voice>
+                            <voice name='${font}' xml:lang='${locale}' xml:gender='${gender}'>${text}</voice>
                             </speak>`;
 
                 let baseRequest = request.defaults({
@@ -102,7 +136,7 @@ export class BingSpeechClient {
                         'Content-Length': ssml.length,
                         'X-Microsoft-OutputFormat': this.AUDIO_OUTPUT_FORMAT,
                         'X-Search-AppId': '00000000000000000000000000000000',
-		                    'X-Search-ClientID': '00000000000000000000000000000000',
+                        'X-Search-ClientID': '00000000000000000000000000000000',
                         'User-Agent': 'bingspeech-api-client'
                     },
                     timeout: 15000,
@@ -147,4 +181,13 @@ export class BingSpeechClient {
                       .map((c) => '&#' + c.charCodeAt(0) + ';')
                       .join('');
     }
+}
+
+/**
+ * Get the appropriate voice font
+ * see https://www.microsoft.com/cognitive-services/en-us/Speech-api/documentation/API-Reference-REST/BingVoiceOutput#SupLocales
+ */
+function voiceFont(locale: string, gender: string): string {
+    let voiceKey = (locale + ' ' + gender).toLowerCase();
+    return VOICES[voiceKey];
 }
